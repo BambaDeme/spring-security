6c6c59415b6d64c0473552e1514669e984be5802
bambademe <cheikhbambademe@gmail.com>
2022-11-10 00:17:19 +0000

    finishing controller

--- src/main/java/com/deme/ahmadou/security/SpringSecurityApplication.java
+++ src/main/java/com/deme/ahmadou/security/SpringSecurityApplication.java
@@ -1,7 +1,14 @@
 package com.deme.ahmadou.security;
 
+import com.deme.ahmadou.security.models.Role;
+import com.deme.ahmadou.security.models.User;
+import com.deme.ahmadou.security.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class SpringSecurityApplication {
@@ -10,4 +17,27 @@ public class SpringSecurityApplication {
 		SpringApplication.run(SpringSecurityApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner run(UserService userService){
+		return args -> {
+			userService.saveRole(new Role(null,"ROLE_USER"));
+			userService.saveRole(new Role(null,"ROLE_MANAGER"));
+			userService.saveRole(new Role(null,"ROLE_ADMIN"));
+			userService.saveRole(new Role(null,"ROLE_SUPER_ADMIN"));
+
+			userService.saveUser(new User(null,"Bamba DEME","bamba","1234",new ArrayList<>()));
+			userService.saveUser(new User(null,"Sourakhatou DIENE","sourah","1234",new ArrayList<>()));
+			userService.saveUser(new User(null,"Diana DIABONG","diana","1234",new ArrayList<>()));
+			userService.saveUser(new User(null,"Mame Cheikh Seck","mamecheikh","1234",new ArrayList<>()));
+
+			userService.addRoleToUser("bamba","ROLE_USER");
+			userService.addRoleToUser("bamba","ROLE_ADMIN");
+			userService.addRoleToUser("bamba","ROLE_SUPER_ADMIN");
+			userService.addRoleToUser("mamecheikh","ROLE_ADMIN");
+			userService.addRoleToUser("diana","ROLE_MANAGER");
+			userService.addRoleToUser("sourah","ROLE_USER");
+
+
+		};
+	}
 }
--- src/main/java/com/deme/ahmadou/security/controllers/UserController.java
+++ src/main/java/com/deme/ahmadou/security/controllers/UserController.java
@@ -1,13 +1,15 @@
 package com.deme.ahmadou.security.controllers;
 
+import com.deme.ahmadou.security.models.Role;
 import com.deme.ahmadou.security.models.User;
 import com.deme.ahmadou.security.service.UserService;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -20,4 +22,32 @@ public class UserController {
     public ResponseEntity<List<User>> getUsers(){
         return ResponseEntity.ok().body(userService.getUsers());
     }
+
+    @GetMapping("/users/{userName}")
+    public ResponseEntity<User> getUser(@PathVariable String userName){
+        return ResponseEntity.ok().body(userService.getUser(userName));
+    }
+
+    @PostMapping("/user/save")
+    public ResponseEntity<User> saveUser(@RequestBody User user){
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/v1/user/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveUser(user));
+    }
+
+    @PostMapping("/role/save")
+    public ResponseEntity<Role> saveRole(@RequestBody Role role){
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/v1/role/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveRole(role));
+    }
+
+    @PostMapping("/role/addToUser")
+    public ResponseEntity<?> addRoleToUser(@RequestBody RoleUserForm roleUserForm ){
+        userService.addRoleToUser(roleUserForm.getUserName(),roleUserForm.getRoleName());
+        return ResponseEntity.ok().build();
+    }
+}
+@Data
+class RoleUserForm{
+    private String userName;
+    private String roleName;
 }
